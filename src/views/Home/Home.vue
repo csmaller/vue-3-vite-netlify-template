2d3c690635bf2dd39220a81a78a46e730b712e3f
corey <csmaller@elumatherapy.com>
2023-05-18 15:12:41 -0600

    background image for phones

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts"></script>
 
-<template><div class="flex w-full main-img">hadsfdsa</div></template>
+<template><div class="flex w-full main-img"></div></template>
 
 <style scoped lang="scss">
 .main-img {
   background-image: url(/src/assets/swimmers.png);
   height: 670px;
   border-top: 6px solid $orange;
-  background-size: 100% 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+
+  @media (max-width: 768px) {
+    height: 420px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    height: 210px;
+  }
 }
 </style>

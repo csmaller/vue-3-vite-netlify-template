fa7ca30597e28ffefa25ee447211e3b8982303fd
corey <csmaller@elumatherapy.com>
2023-05-18 09:14:27 -0600

    trying again with hidden field

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -135,7 +135,8 @@ const doToast = () => {
       <Button id="save_btn" type="submit" label="Send" :disabled="v$.$invalid" class="button" />
     </div>
   </form> -->
-  <form name="motive8contact" method="POST" data-netlify="true" action="/#">
+  <form name="motive8contact" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="motive8contact" />
     <input name="contact" />
     <button name="submit" type="submit">Submits</button>
   </form>

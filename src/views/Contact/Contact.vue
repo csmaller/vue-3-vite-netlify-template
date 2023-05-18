642cfd5fd8e1b43411c2d2e78813431ef53fd808
corey <csmaller@elumatherapy.com>
2023-05-18 07:47:37 -0600

    trying again

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -97,7 +97,7 @@ const doToast = () => {
 </script>
 
 <template>
-  <!-- <form
+  <form
     id="myForm"
     name="motive8contact"
     method="post"
@@ -134,38 +134,6 @@ const doToast = () => {
       </div>
       <Button id="save_btn" type="submit" label="Send" :disabled="v$.$invalid" class="button" />
     </div>
-  </form> -->
-  <form name="motive8contact" method="POST" data-netlify="true">
-    <p>
-      <label>
-        Your Name:
-        <input type="text" name="name" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Email:
-        <input type="email" name="email" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Role:
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-      </label>
-    </p>
-    <p>
-      <label>
-        Message:
-        <textarea name="message"></textarea>
-      </label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
   </form>
 </template>
 
